ea1835f407e01c049ae1901e5256696c0d0ceb58
Cynthia Taylor <820@holbertonschool.com>
2020-02-07 21:28:47 +0000

    Add final task for project 0x00

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int buf = 59;
+
+	write(2, str, buf);
+
+	return (0);
+}
