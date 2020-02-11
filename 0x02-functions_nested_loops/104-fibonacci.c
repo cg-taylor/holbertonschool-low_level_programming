868a7ba6309084b83bbddda9092a594b51076231
Cynthia Taylor <820@holbertonschool.com>
2020-02-11 22:04:53 +0000

    First attempt at final advanced task

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <float.h>
+
+/**
+ * main - Print the first 98 numbers in the Fibonacci sequence
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	float num1, num2, temp;
+	int count = 1;
+
+	num1 = 1;
+	num2 = 2;
+
+	printf("%.f, ", num1);
+
+	for ( ; count < 98; count++)
+	{
+		if (count != 97)
+			printf("%.f, ", num2);
+		else
+			printf("%.f\n", num2);
+
+		temp = num2;
+		num2 += num1;
+		num1 = temp;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
