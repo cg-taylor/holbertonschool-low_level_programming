4f0d8a64d04b96358a5a26370c21c6861b0ce9c8
Cynthia Taylor <820@holbertonschool.com>
2020-02-13 21:51:34 +0000

    Submit first working attempt at advanced task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * find_prime - finds the largest prime factor of a number
+ * @num: the number to factor
+ * @max: the square root of the number and upper limit for tests
+ *
+ * Return: largest prime factor
+ */
+
+long find_prime(long num, long max)
+{
+	long factor, maxPrime;
+
+	for (factor = 3; factor <= max; factor += 2)
+	{
+		if (num % factor == 0)
+		{
+			maxPrime = factor;
+			num /= factor;
+		}
+	}
+
+	if (num > 2)
+		maxPrime = num;
+
+	return (maxPrime);
+
+}
+
+/**
+ * main - print the largest prime factor of a number
+ *
+ * Return: Always 1
+ */
+
+int main(void)
+{
+	long number = 612852475143;
+	long factor, min;
+
+	min = (long) sqrt(612852475143);
+	factor = find_prime(number, min);
+
+	printf("%ld\n", factor);
+
+	return (0);
+}
