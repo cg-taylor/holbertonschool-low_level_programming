63d6af6996331187458b7283fe564de7edd60333
Cynthia Taylor <820@holbertonschool.com>
2020-02-24 21:17:50 +0000

    Fix NULL parameter test

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,7 +14,7 @@ char *_strstr(char *haystack, char *needle)
 	char *found = NULL;
 	int idx;
 
-	if (!*haystack || !*needle)
+	if (!haystack || !needle)
 		return (NULL);
 
 	for ( ; *haystack; haystack++)
