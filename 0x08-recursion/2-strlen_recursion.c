71151c1d4010e89bc802f2134041d49020539dfd
Cynthia Taylor <820@holbertonschool.com>
2020-02-26 21:35:12 +0000

    Add tasks 2 and 3

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * calcLength - calculate the length of a string
+ * @str: a pointer to a string
+ * @length: the length of the string
+ *
+ * Return: the length of the string
+ */
+
+int calcLength(char *str, int length)
+{
+	if (*str)
+		return (calcLength(str + 1, length + 1));
+	return (length);
+}
+
+/**
+ * _strlen_recursion - determine the length of a string using recursion
+ * @s: pointer to the beginning of a string
+ *
+ * Return: the length of a string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (!s)
+		return (0);
+	else if (!*s)
+		return (1);
+	else
+		return (calcLength(s, 0));
+}
---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * calcFactorial - does the math to calculate the factorial of an integer
+ * @product: the factorial of `n`
+ * @n: an integer
+ *
+ * Return: the factorial of `n`
+ */
+
+int calcFactorial(int product, int num)
+{
+	if (num)
+	{
+		product *= num;
+		return (calcFactorial(product, num - 1));
+	}
+	return (product);
+}
+
+/**
+ * factorial - calculates the factorial of an integer
+ * @n: an integer
+ *
+ * Return: the factorial of an integer as an integer, -1 if n < 0
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	else
+		return (calcFactorial(1, n));
+}
