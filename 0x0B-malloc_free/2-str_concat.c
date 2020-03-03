e078f5f23bd5683e35a37706d36c7a08e251d041
Cynthia Taylor <820@holbertonschool.com>
2020-03-03 20:39:04 +0000

    Replace NULL parameter conditional with different condition and response

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -21,7 +21,7 @@ int _strlen(char *str)
 }
 
 /**
- * str_concat - concatenates two strings in a dynamically allocated memory buffer
+ * str_concat - concatenates two strings in dynamically allocated buffer
  * @s1: pointer to the first string
  * @s2: pointer to the second string
  *
@@ -31,24 +31,26 @@ int _strlen(char *str)
 char *str_concat(char *s1, char *s2)
 {
 	char *new = NULL;
-	int length1, length2, idx;
+	int idx, length1, length2;
 
-	if (!s1 && !s2)
-		return (NULL);
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
 
 	length1 = _strlen(s1);
 	length2 = _strlen(s2);
 
-	new = malloc(sizeof(char) * (length1 + length2));
+	new = malloc(sizeof(char) * (length1 + length2 + 1));
 
 	if (!new)
 		return (NULL);
 
-	for (idx = 0; s1[idx]; idx++)
-		new[idx] = s1[idx];
+	for (idx = 0; *s1; idx++, s1++)
+		new[idx] = *s1;
 
-	for ( ; s2[idx - length1]; idx++)
-		new[idx] = s2[idx - length1];
+	for ( ; *s2; idx++, s2++)
+		new[idx] = *s2;
 
 	new[idx] = '\0';
 
