4deff3650fd8c2409fdf5337cb95b8dfdf7a42f2
Cynthia Taylor <820@holbertonschool.com>
2020-03-06 02:02:59 +0000

    Add first attempt at task 3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * array_range - create and fill an array of integers
+ * @min: the number to start filling the array from
+ * @max: the last number to store in the array
+ *
+ * Description: The array contains all values from `min` to `max` (inclusive) in
+ *              numerical order.
+ *
+ * Return: pointer to the new array or NULL if it fails
+ */
+
+int *array_range(int min, int max)
+{
+	int *array = NULL;
+	int idx;
+
+	if (min > max)
+		return (NULL);
+
+	array = malloc(sizeof(int) * (1 + max - min));
+
+	if (!array)
+		return (NULL);
+
+	for (idx = 0; min <= max; idx++, min++)
+	{
+		array[idx] = min;
+	}
+
+	return (array);
+}
