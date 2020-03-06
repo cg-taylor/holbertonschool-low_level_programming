bcad65bf017ac4e57a06ab0d904cbb3d218b30bc
Cynthia Taylor <820@holbertonschool.com>
2020-03-06 00:47:20 +0000

    First attempt at task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,77 @@
+#include "holberton.h"
+
+/**
+ * _strlen - Determine the length of a string
+ * @str: pointer to a string
+ *
+ * Return: the number of characters in the string
+ */
+
+int _strlen(char *str)
+{
+	int length;
+
+	for (length = 0; str[length]; length++)
+		;
+
+	return (length);
+}
+
+/**
+ * string_nconcat - Concatenate two strings
+ * @s1: pointer to the first string
+ * @s2: pointer to the second string
+ * @n: number of bytes to append from `s2`
+ *
+ * Description: Allocates a new space in memory to hold `s1` and `n` bytes of
+ *              `s2`. The new string is null terminated even if all of `s2` is
+ *              not included. A NULL string is treated like an empty string.
+ *
+ * Return: pointer to the new string or NULL if it fails
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *new = NULL;
+	unsigned int i, length1, length2, total_length;
+
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
+
+	length1 = _strlen(s1);
+	length2 = _strlen(s2);
+
+/*
+ * If n >= length2, then change to n = length2 so that we don't continue adding
+ * random unknown characters to the new string
+ */
+	if (n > length2)
+		n = length2;
+
+/* Extra byte for the null terminator is included in length1 */
+	total_length = length1 + n;
+
+	new = malloc(sizeof(char) * total_length);
+
+	if (!new)
+		return (NULL);
+
+	for (i = 0; i < total_length; i++)
+	{
+		if (i < length1 && *s1)
+		{
+			new[i] = *s1;
+			s1++;
+		}
+		else
+		{
+			new[i] = *s2;
+			s2++;
+		}
+	}
+	new[i] = '\0';
+
+	return (new);
+}
