ce457c8877d225d79dbb5b8f107fe1e3586d8970
Cynthia Taylor <820@holbertonschool.com>
2020-03-19 23:35:44 +0000

    First attempt at task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * _strlen - count number of characters in a string
+ * @str: a string
+ *
+ * Return: number of characters in the string
+ */
+
+int _strlen(char *str)
+{
+	int count;
+
+	for (count = 0; str[count]; count++)
+		;
+	return (count);
+}
+
+/**
+ * add_node - add a new node to the beginning of a singly linked list
+ * @head: double pointer to the head of a singly linked list
+ * @str: the contents of the new node
+ *
+ * Return: pointer to the new element
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->len = _strlen(new->str);
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
