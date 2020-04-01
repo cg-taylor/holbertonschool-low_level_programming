404234cf65336776e4660f6cc469ff3052fe8a1a
Cynthia Taylor <820@holbertonschool.com>
2020-04-01 19:30:34 +0000

    Moved text_content test back to before _strlen call

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -34,14 +34,14 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
+	if (!text_content)
+		text_content = "";
+
 	length = _strlen(text_content);
 
 	fd = open(filename, O_RDWR | O_APPEND);
 	if (fd == -1)
 		return (-1);
-
-	if (!text_content)
-		return (1);
 
 	bytes_written = write(fd, text_content, length);
 	if (bytes_written == -1)
