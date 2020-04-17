fc4358a34af8bf49b7b2cb4dd24f95eac7e5e528
Cynthia Taylor <820@holbertonschool.com>
2020-04-17 23:37:30 +0000

    Add all files for task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,34 +1,33 @@
 #include "3-calc.h"
-
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * get_op_func - select the correct operation function
- * @s: the operator passed to the program as an argument
- *
- * Return: pointer to a function that takes two integers as parameters
- */
-
+  * get_op_func - choosing the right func for the operator
+  * @s: the operator
+  *
+  * Return: result of function or error
+  */
 int (*get_op_func(char *s))(int, int)
 {
+	/* declarations */
 	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
 	};
-	int i = 0;
-
-	if (!s)
-		return (NULL);
-
+	int i;
+	/* matches operator to function */
+	i = 0;
 	while (ops[i].op)
 	{
-		if (*s == ops[i].op[0] && !s[1])
+		if (ops[i].op[0] == *s && s[1] == '\0')
 			return (ops[i].f);
 		i++;
 	}
-
+	/* bad operator, print error */
 	printf("Error\n");
 	exit(99);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,35 +1,26 @@
 #include "3-calc.h"
-
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * main - entry point for a program that performs simple operations
- * @argc: number of command line arguments
- * @argv: array containing all command line arguments
- *
- * Return: 0 if successful
- *         Exit status 98 if the number of arguments is incorrect
- *         Exit status 99 if the operator is invalid
- *         Exit status 100 if user attempts to divide by 0
- */
-
+  * main - entry point
+  * @argc: # of args
+  * @argv: array of pointers to names of args
+  * Return: 0
+  */
 int main(int argc, char **argv)
 {
-	int num1, num2, result;
-	int (*op_func)(int, int);
-
-	/* check number of arguments */
+	/* input checking */
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-
-	op_func = get_op_func(argv[2]);
-
-	result = op_func(num1, num2);
-
-	printf("%d\n", result);
+	if ((*argv[2] == '/' || *argv[2] == '%') && atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	/* calling get_op_func with arguments */
+	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
