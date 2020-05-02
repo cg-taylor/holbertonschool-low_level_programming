331f0da40a758e9058155b8a9a50cf38634471cd
Cynthia Taylor <820@holbertonschool.com>
2020-05-02 22:57:36 +0000

    Add first attempt at tasks 1 and 2

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - Count the number of elements in a dlistint_t list
+ * @h: pointer to the head of a dlistint_t list
+ *
+ * Return: the number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t num_nodes = 0;
+
+	if (!h)
+		return (0);
+
+/* Make sure h is pointing to the first node */
+	if (h->prev != NULL)
+	{
+		while(h->prev)
+			h = h->prev;
+	}
+
+	while (h)
+	{
+		num_nodes++;
+		h = h->next;
+	}
+
+	return (num_nodes);
+}
